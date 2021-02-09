0631a05a78152317f6ea1c6f9510766741088b26
Allan Bowe <allan@bowe.io>
2021-02-10 00:12:49 +0100

    chore(docs): adding a homepage to the doc site and integrating with the sasjs doc command.  See https://core.sasjs.io

---
+++ sasjs/utils/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+####################################################################
+# PROJECT: Macro Core Docs Build                                   #
+####################################################################
+
+sasjs doc
+
+# refresh github pages site
+rm -rf sasjsbuild/docsite
+git clone git@github.com:sasjs/core.github.io.git sasjsbuild/docsite
+rm -rf sasjsbuild/docsite/*
+mv sasjsbuild/docs/* sasjsbuild/docsite/
+cd sasjsbuild/docsite/
+echo 'core.sasjs.io' > CNAME
+git add .
+git commit -m "build.sh build on $(date +%F:%H:%M:%S)"
+git push
+npx sitemap-generator-cli https://core.sasjs.io
+git add .
+git commit -m "adding sitemap"
+git push
+
+echo "check it out:  https://sasjs.github.io/core.github.io/files.html"
